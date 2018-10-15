cbb0cc49069e7a461946743a96ab7017a0532379
Andrew Emmons <andrewcemmons@gmail.com>
2018-10-15 13:52:59 -0700

    Adds React build artifact to repo

---
+++ frontend/build/precache-manifest.7195980eb31c04ef6da715d9aa1d4cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "72a4330adc3c6bd31015",
+    "url": "/static/js/main.72a4330a.chunk.js"
+  },
+  {
+    "revision": "554bf1a48d9f34f690d6",
+    "url": "/static/js/1.554bf1a4.chunk.js"
+  },
+  {
+    "revision": "72a4330adc3c6bd31015",
+    "url": "/static/css/main.98bba8a7.chunk.css"
+  },
+  {
+    "revision": "07f9879d24649c92bf07e0f6db01ee4d",
+    "url": "/index.html"
+  }
+];
